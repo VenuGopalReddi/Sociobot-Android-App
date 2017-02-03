0d74db0a45683ee22d7b05a9d0605d5233770e17
VenuGopalReddi <venugopal_reddy.modhugu@students.tamuk.edu>
2017-02-03 02:25:56 -0600

    project upload

---
+++ app/src/main/java/sociobot/Value.java
@@ -0,0 +1,27 @@
+
+
+import android.graphics.drawable.Drawable;
+/**
+ * Created by venugopal on 11/27/2015.
+ */
+public class Value {
+
+    public int image;
+    public String value;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
